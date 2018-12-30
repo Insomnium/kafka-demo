74bb194cfe89a7f183a5a07ca30a81a194d54d96
ins <Ins137@gmail.com>
2018-12-31 00:13:39 +0400

    add micrometer and another samlpe controller endpoint

--- src/main/java/net/ins/kafkademo/controller/LimitsController.java
+++ src/main/java/net/ins/kafkademo/controller/LimitsController.java
@@ -5,9 +5,11 @@ import net.ins.kafkademo.domain.Limit;
 import net.ins.kafkademo.service.LimitsService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/limits")
@@ -19,5 +21,10 @@ public class LimitsController {
     @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Limit> listLimits() {
         return limitsService.listLimits();
+    }
+
+    @GetMapping("/{account}")
+    public Mono<Limit> limitByAccount(@PathVariable("account") String account) {
+        return limitsService.findByAcc(account);
     }
 }
--- src/main/java/net/ins/kafkademo/service/LimitsService.java
+++ src/main/java/net/ins/kafkademo/service/LimitsService.java
@@ -10,4 +10,5 @@ import java.math.BigDecimal;
 public interface LimitsService {
     Flux<Limit> listLimits();
     Mono<Limit> setupLimit(String account, BigDecimal min, BigDecimal max);
+    Mono<Limit> findByAcc(String account);
 }
